9405b4b8f33f5c14a051d32e2bab5c63b7834145
alexvillalb159 <38950117+alexvillalb159@users.noreply.github.com>
2022-12-22 14:42:37 -0400

    Create product.php

---
+++ product.php
@@ -0,0 +1,48 @@
+<?php
+
+function getProduct($product_id) {
+	// curl -X GET  -H 'Authorization: Bearer $GLOBALS['ACCESSTOKEN']' https://api.mercadolibre.com/items/$ITEM_ID?include_attributes=all
+	$ch = curl_init();	
+	curl_setopt($ch, CURLOPT_URL, "https://api.mercadolibre.com/items/" . $product_id . "?include_attributes=all");	
+	$headers = array(
+		    "Authorization: Bearer " . $_COOKIE['access_token']
+	);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+
+	$result = json_decode(curl_exec($ch)); 
+	curl_close($ch);
+	return $result;
+
+
+}
+
+
+function get20BestSellingProductsID($country_id, $category_id) {
+
+	// 20 productos más vendidos
+	// curl -X GET -H 'Authorization: Bearer $_COOKIE['access_token'] https://api.mercadolibre.com/highlights/$SITE_ID/category/$CATEGORY_ID
+
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://api.mercadolibre.com/highlights/" . $country_id . "/category/" . $category_id);
+
+	$headers = array(
+		    "Authorization: Bearer " . $_COOKIE['access_token']
+	);
+
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+
+	$result = json_decode(curl_exec($ch)); 
+	curl_close($ch);
+	return $result;
+
+}
+
+
+
+
+?>
