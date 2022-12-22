f291a0400792aa7b9bba4ebc3b33ba61f409ca5a
alexvillalb159 <38950117+alexvillalb159@users.noreply.github.com>
2022-12-22 14:40:22 -0400

    Update mercadolibre.php

--- mercadolibre.php
+++ mercadolibre.php
@@ -14,19 +14,200 @@
 
 ?>
 
+<!DOCTYPE html>
 <html>
 <head>
+<title>API Mercado Libre</title>
+
+<style>
+	body {
+		
+		margin-top:20px;
+		margin-left: 20px;
+		background-color: aquamarine;
+		display: grid;
+		grid-template-columns: 900px 200px;
+		justify-content: center;
+		grid-auto-rows: 35px;
+	}
+
+
+	div.line {
+		grid-column-start: 1;
+		grid-column-end: 3;
+		display: flex;
+		align-items: center;
+	}
+
+	div.titule2 {
+		grid-column-start: 1;
+		grid-column-end: 2;
+		text-align: center;
+		font-weight: bold;
+	}
+
+	h4 {
+		text-align: center;
+		grid-column-start: 1;
+		grid-column-end: 2;
+	}
+
+
+	form {
+		
+		display: grid;
+		grid-template-columns: 320px 320px;
+		grid-column-start: 1;
+		grid-column-end: 2;
+		grid-auto-rows: 30px;
+		grid-row-start: 3
+
+	} 
+	
+	
+	label {
+
+  		text-align: end;
+  		margin-right: 5px;
+	}	
+	select {
+		width:200px;
+		margin-bottom: 6px;	
+		
+	}
+	input[type=text] {
+
+		width: 300px;
+		margin-bottom: 6px;
+		
+	}
+	input.post {
+		grid-column-start: 1;
+  		grid-column-end: 2;
+  		width: 150px;
+  		margin: auto;
+	
+	}
+	
+	table.products {
+		display: table;
+		width: 700px;
+		border: 1px solid black;
+		border-radius: 10px;
+		grid-column-start: 1;
+  		grid-column-end: 2;
+
+	}
+	table.products th {
+		border: 1px solid black;
+		/* border-radius: 10px; */
+
+	}
+	
+	table.products td {
+		border: 1px solid black;
+		/* border-radius: 10px; */
+
+	}
+	
+</style>
+
+<script>
+	var urlroot = window.location.origin; 
+	var country_codes = {
+		"Argentina": "MLA", 
+		"Brasil": "MLB", 
+		"Colombia": "MCB", 
+		"Costa Rica": "MCR", 
+		"Ecuador": "MEC",
+		"Chile": "MLC", 
+		"México": "MLM", 
+		"Panamá": "MPA", 
+		"Perú": "MPE", 
+		"Portugal": "MPT", 
+		"República Dominicana": "MRD",
+		"Uruguay": "MLU", 
+		"Venezuela": "MLV"
+	};
+
+	function countryOnChangeEvent()
+	{
+		window.esperar.showModal();
+
+		var selectedValue = document.getElementById('country').value;
+		// Hace una solicitud sincrona
+		let xhr = new XMLHttpRequest();
+		try {
+			xhr.open("GET", urlroot + "/20masvendidos.php?" + "country_id=" + country_codes[selectedValue] + "&category_id=" + 
+				country_codes[selectedValue] + "1648", false);
+ 
+
+
+
+			xhr.send(); 
+
+			if (xhr.readyState === 4) {
+	     			console.log(xhr.responseText);
+				products = JSON.parse(xhr.responseText);
+				printProducts(products);
+
+			}
+		} catch (err ) {
+			console.log("Exceción: " + err);
+		}
+		
+		window.esperar.close();
+		// Quita el foco de la lista de países 
+		document.getElementById("country").blur();
+		//document.getElementById("products").focus();;
+
+		
+	}
+	
+	function printProducts(products) {
+		var table = document.getElementById("products");
+		deleteTable(table);
+
+		for(i=0; i < products.length; i++) {
+			// Create an empty <tr> element and add it to the 1st position of the table:
+			var row = table.insertRow(i+1);
+
+			if( typeof(products[i].error) === "undefined") {
+				// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+				var position = row.insertCell(0);
+				var id = row.insertCell(1);
+				var title = row.insertCell(2);
+				var price = row.insertCell(3);
+				
+				position.innerHTML = "" + (i+1);
+				id.innerHTML = products[i].id; 
+				title.innerHTML = products[i].title; 
+				price.innerHTML = products[i].price; 
+			} else {
+				var position = row.insertCell(0);
+				var id = row.insertCell(1);
+				position.innerHTML = "" + (i+1);
+				id.innerHTML = products[i].message; 
+			
+			}
+		}
+	
+	}
+	
+	function deleteTable(table) {
+		while(table.rows.length > 1 ) table.deleteRow(1);
+	}
+
+</script>
+
 
 <?php
 
+
+
+
 $URLAPP="";
 
-$options = [
-	'authSite' => '',
-	'clientId' => '',
-	'clientSecret' => '',
-	'redirectUri' => ''
-];
 
 $authSites = array(
 	"Argentina" =>			array( "code" =>"MLA", "url" =>"https://auth.mercadolibre.com.ar" ),
@@ -45,10 +226,7 @@ $authSites = array(
 
 );
 
-/*
-$paises = array_keys($authSites );
-echo "País: " . $paises[0]. "<br>";
-echo "Argentina: " . $authSites['Argentina']['code'] . "<br>"; */
+
 
 
 function printJSON($result) {
@@ -62,24 +240,79 @@ function printJSON($result) {
 
 }
 
-function getCategories($access_token , $country_code) {
+function printProducts ($products ) {
+
+	echo 	"<table border='1'>";
+	echo 	"<tr>" . 
+		"<th>Posicion</th>" . 
+		"<th>ID</th>" . 
+		"<th>Título</th>" . 
+		"<th>Precio</th>" . 
+		"</tr>"; 
+	
+
+	$i=1;
+	foreach($products as $product) {
+		if(!isset($product->error)) {
+			echo "<tr>"; 
+			echo "<td>" . $i . "</td>"; 
+			echo "<td>" . $product->id . "</td>"; 
+			echo  "<td>" . $product->title . "</td>";
+			echo "<td>" . $product->price . "</td>"; 
+			echo "</tr>";
+		} else {
+			echo "<tr>"; 
+			echo "<td>" . $i . "</td>";
+			echo "<td>" .  $product->message . "</td>";
+			echo "<td></td>";
+			echo "<td></td>";
+			//echo "<td>" . "Error: producto no exhibible" . "</td>"; 
+			echo "</tr>";
+		}
+		$i++;
+	}
+	echo "</table>";
+
+	
+}
+
+
+
+
+
+
+function getCategories($country_code) {
 
 	$ch = curl_init();
 
 	// Toma todas las categorias
-	// curl -X GET -H 'Authorization: Bearer $ACCESS_TOKEN'   https://api.mercadolibre.com/sites/MLA/categories
+	// curl -X GET -H 'Authorization: Bearer ['ACCESSTOKEN']'   https://api.mercadolibre.com/sites/MLA/categories
 
 	curl_setopt($ch, CURLOPT_URL, 'https://api.mercadolibre.com/sites/' . $country_code . '/categories');
 	$headers = array(
-		    "Authorization: Bearer " . $access_token
+		    "Authorization: Bearer " . $GLOBALS['ACCESSTOKEN']
 	);
-
+  
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	$result = curl_exec($ch); 
 	
 	$resultjson = json_decode($result);
 	return $resultjson;
+}
+
+function printHTMLSelectCountry() {
+	global $authSites;
+	
+	echo '<label for="fname"><b>País:</b></label>';
+	
+	echo '<select name="country" id="country" required>';
+	echo '	<option value="" selected></option>';
+
+	foreach( $authSites as $key => $val ) {
+		echo "<option value='" .$key . "'>" .$key . "</option>"; 
+	} 
+	echo '</select>';
 }
 
 
@@ -100,6 +333,8 @@ if(isset($_GET['allright'])) {
 	exit("");
 	
 }
+
+
 ?>
 
 <!--
@@ -110,60 +345,6 @@ if(isset($_GET['allright'])) {
 </script>
 -->
 
-<style>
-
-	h2 {
-		text-align: center;
-	}
-
-	body {
-		
-		margin-top:20px;
-		margin-left: 100px;
-		background-color: aquamarine;
-	}
-
-	form {
-		
-		display: grid;
-		grid-template-columns: 200px 400px;
-		margin-left: inherit;
-
-	} 
-	div {
-	}	
-	
-	label {
-		
-		grid-column-start: 1;
-  		grid-column-end: 2;
-  		text-align: end;
-  		margin-right: 5px;
-	}	
-	select {
-		width:200px;
-		margin-bottom: 6px;	
-		grid-column-start: 2;
-  		grid-column-end: 3;
-		
-	}
-	input[type=text] {
-
-		width: 300px;
-		margin-bottom: 6px;
-		grid-column-start: 2;
-  		grid-column-end: 3;
-		
-	}
-	.post {
-		grid-column-start: 1;
-  		grid-column-end: 3;
-  		width: 150px;
-  		margin: auto;
-	
-	}
-	
-</style>
 
 </head>
 
@@ -185,31 +366,21 @@ if(isset($_GET['code'])) {
 			'client_secret' => $_COOKIE['client_secret'],
 			'code' => ''.$_GET["code"] .'',
 			'redirect_uri' => $URLAPP . '/mercadolibre.php' 
-	//		'code_verifier' => '$CODE_VERIFIER' 
+
 	);
 
 
 	curl_setopt($ch, CURLOPT_URL, "https://api.mercadolibre.com/oauth/token");
 	curl_setopt($ch, CURLOPT_POST, TRUE);
-	#curl_setopt($ch, CURLOPT_FILE, $fp);
-	#curl_setopt($ch, CURLOPT_HEADER, TRUE);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 	#curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-	//echo "<br>Length: ". strlen(implode($data)) . "<br>";
 
 
-	/*
-	curl_setopt($ch, CURLOPT_HTTPHEADER, 
-			array(	"accept: application/json",
-				"content-type: multipart/form-data",
-				"content-length: " . strlen(implode($data))
-			)
-				
-		); 
-	*/
+
+
 			
 
 	$result = curl_exec($ch);
@@ -220,39 +391,55 @@ if(isset($_GET['code'])) {
 	}
 	else {
 		$resultjson = json_decode($result);
-		echo "APP token: " . $resultjson->access_token . "<br>";  
+
+		setcookie("access_token", $resultjson->access_token, time() + (86400 * 30), "/");
+
 	}
 
+	curl_close($ch);
+	
+	echo "<h4>Mercado Libre API</h4>";  
+	echo "<div class='line' ><p><b>APP ID:</b> " .  $_COOKIE['client_id'] . "</p></div>";  
+	echo "<div class='line' ><p><b>APP token:</b> " . $resultjson->access_token . "</p></div>";  
+	echo "<div class='line'>";
+	printHTMLSelectCountry();
+	echo "</div>";
 
-	echo "20 productos más vendidos de Computación en Argentina:<br>";
+	
+	echo "<script>";
+	echo "document.getElementById('country').addEventListener('change', countryOnChangeEvent);"; 
+	echo "</script>";
+	
+	echo "<div class='titule2'>20 productos más vendidos de Computación:</div>";
+
+	echo 	"<table class='products' id='products'>";
+	echo 	"<tr>" . 
+		"<th>Posicion</th>" . 
+		"<th>ID</th>" . 
+		"<th>Título</th>" . 
+		"<th>Precio</th>" . 
+		"</tr>"; 
+	echo 	"</table>";
+
+	echo 	"<dialog id='esperar'>" .
+	      	"<h2>Espere</h2>" .
+		"</dialog>";
 
 
-	// 20 productos más vendidos
-	// curl -X GET -H 'Authorization: Bearer $ACCESS_TOKEN' https://api.mercadolibre.com/highlights/$SITE_ID/category/$CATEGORY_ID
-
-
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://api.mercadolibre.com/highlights/MLA/category/MLA1648");
-
-	$headers = array(
-		    "Authorization: Bearer " . $resultjson->access_token 
-	);
-
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-
-	$result = json_decode(curl_exec($ch)); 
-
-
+//	$products = get20BestSellingProducts("MLA", "MLA1648");
+//	printProducts ($products );
+	
+	//echo "Nombre: " . $products[0]['title'];
+/*
 	if(isset($result->error)) {
-		echo "<br>Error: " . $result->message . "<br>";
+		echo "Error: " . $result->message ;
 		//echo  curl_error($ch);
 	} else {
 		printJSON(json_encode($result));
 	}
 
-	curl_close($ch);
+*/
+
 
 
 	
@@ -266,26 +453,20 @@ if(isset($_GET['code'])) {
 
 
 
-<h2>Mercado Libre API</h2>
+<h4>Mercado Libre API</h4>
 
 <form action="mercadolibre.php?allright" method="post">
-	<label for="fname">País:</label>
-	<select name="country" id="country" required>
-		<option value='' selected></option>
-		<?php
-			foreach( $authSites as $key => $val ) {
-				echo "<option value='" .$key . "'>" .$key . "</option>"; 
-			} 
-		
-		?>
-	
-	</select>
+	<?php 
+		printHTMLSelectCountry();
+	?>
 
-	<label for="fname">Client ID:</label>
-	<input type="text" id="client_id" name="client_id" required><br>
-	<label for="client_secret">Client Secret:</label>
+	<label for="fname"><b>Client ID:</b></label>
+	<input type="text" id="client_id" name="client_id" required>
+	<label for="client_secret"><b>Client Secret:</b></label>
 	<input type="text" id="client_secret" name="client_secret" required>
-	<input type='submit' class="post" name="post" value="Enviar">
+	<div class="line">
+		<input type='submit' class="post" name="post" value="Enviar">
+	</div>
 
 </form> 
 
