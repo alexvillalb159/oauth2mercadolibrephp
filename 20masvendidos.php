31a46fbe8b390e569a0339cd34c2e259ab7ac770
alexvillalb159 <38950117+alexvillalb159@users.noreply.github.com>
2023-01-04 15:18:37 -0400

    Update 20masvendidos.php

--- 20masvendidos.php
+++ 20masvendidos.php
@@ -4,22 +4,33 @@ include 'product.php';
 
 function get20BestSellingProducts($country_id, $category_id) {
 	$result = get20BestSellingProductsID($country_id, $category_id); 
-	//echo "PRODUCTS: " . json_encode($result) . "----------------<br>"; 
-	$products = $result->content;
-	$i=0;
-	foreach( $products as $product ) {
-		$id = $product->id;
-		$product_item= getProduct($id);	
-		//echo "BARBARO: " . json_encode($product_item)  . " ------------- <br>"; 
-		$product_items[$i] = $product_item;
-		$i++;
-	} 
-	return $product_items;
+
+	if (isset($result->content)) {
+		$products = $result->content;
+		$i=0;
+		foreach( $products as $product ) {
+			$id = $product->id;
+			$product_item= getProduct($id);	
+
+			$product_items[$i] = $product_item;
+			$i++;
+		} 
+		return $product_items;
+
+	
+	}
+	error_log("La consulta de los 20 mas vendidos no arrojo ningun resultado. Pais: " . $country_id . ", Categoria: ".  $category_id, 0);
+	error_log(	"Mensaje de error: {error: " . $result->error . ", " .
+			"message: " . $result->message . ", " .
+			"status: " . $result->status . "}" );
+	return $result;
 
 }
 
 $result=get20BestSellingProducts($_GET['country_id'], $_GET['category_id'] );
 
 echo json_encode($result);
+
+
 
 ?>
